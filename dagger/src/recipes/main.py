60cbd704102ad77fd6e84a0292a3ab6e32afe0b3
Albert Rise Nielsen <albert@risenielsen.dk>
2025-02-10 22:32:06 +0100

    ci(dagger): init

---
+++ dagger/src/recipes/main.py
@@ -0,0 +1,33 @@
+import this
+import dagger
+from dagger import dag, function, object_type
+
+
+@object_type
+class Recipes:
+    @function
+    def create_container(self, chef_version: str = "v0.10.0") -> dagger.Container:
+        """Returns a container that echoes whatever string argument is provided"""
+
+        chef_url = f"https://github.com/Zheoni/cooklang-chef/releases/download/{chef_version}/chef-x86_64-unknown-linux-musl.tar.gz"
+
+        return (
+            dag.container()
+            .from_("alpine:latest")
+            .with_exec(["apk", "add", "--update", "--no-cache", "curl"])
+            .with_exec(["curl", "-sSL", chef_url, "-o", "/tmp/chef.tar.gz"])
+            .with_exec(["tar", "-xzf", "/tmp/chef.tar.gz", "-C", "/usr/local/bin"])
+            .with_exec(["rm", "/tmp/chef.tar.gz"])
+        )
+
+    @function
+    async def read_recipe(self, source: dagger.Directory, recipe_path: str) -> str:
+        container = self.create_container()
+
+        """Returns lines that match a pattern in the files of the provided Directory"""
+        return await (
+            container.with_mounted_directory("/mnt", source)
+            .with_workdir("/mnt")
+            .with_exec(["chef", "recipe", recipe_path])
+            .stdout()
+        )
