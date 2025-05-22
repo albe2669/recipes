3c89df5a88808c9e77a9b8b9542ceda344161eb5
albe2669 <albert@risenielsen.dk>
2025-05-22 23:55:35 +0200

    refactor(builder): move the builder to its own repo

--- .dagger/src/recipes/main.py
+++ .dagger/src/recipes/main.py
@@ -4,18 +4,21 @@ from dagger import dag, function, object_type
 
 @object_type
 class Recipes:
-    def build_book_builder(self, source: dagger.Directory) -> dagger.File:
+    def get_cooklatex(self) -> dagger.File:
         return (
             dag.container()
             .from_("rust:alpine")
-            .with_exec(
-                ["apk", "add", "--update", "--no-cache", "musl-dev", "alpine-sdk"]
-            )
-            .with_directory("/app", source)
+            .with_exec(["apk", "add", "--update", "--no-cache", "wget"])
             .with_workdir("/app")
-            .with_exec(["cargo", "build", "--release"])
-            # .with_mounted_cache("/app/target", dag.cache_volume("book-builder-target"))
-            .file("/app/target/release/book-builder")
+            .with_exec(
+                [
+                    "wget",
+                    "https://github.com/albe2669/cooklatex/releases/latest/download/cooklatex",
+                    "-O",
+                    "cooklatex",
+                ]
+            )
+            .file("/app/cooklatex")
         )
 
     def build_latex_env(self) -> dagger.Container:
@@ -31,14 +34,14 @@ class Recipes:
         source: dagger.Directory,
     ) -> dagger.File:
         latex_env = self.build_latex_env()
-        book_builder = await self.build_book_builder(source.directory("book-builder"))
+        cooklatex = await self.get_cooklatex()
 
         latex_env = (
             self.build_latex_env()
             .with_directory("/app", source)
             .with_workdir("/app")
-            .with_file("/app/book-builder", book_builder, permissions=0o755)
-            .with_exec(["./book-builder", "-l", "latex", "-o", "/app/out", "./Dinner"])
+            .with_file("/app/cooklatex", cooklatex, permissions=0o755)
+            .with_exec(["./cooklatex", "-l", "template", "-o", "/app/out", "./Dinner"])
             .with_workdir("/app/out")
             .with_exec(["tectonic", "main.tex"])
         )
