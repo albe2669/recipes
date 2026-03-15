08717343403d103cd7bc35613f9a86d4424e3275
albe2669 <albert@risenielsen.dk>
2026-03-15 22:36:29 +0100

    fix: remove pythone

--- flake.nix
+++ flake.nix
@@ -6,13 +6,10 @@
 
   outputs =
     {
-      self,
       nixpkgs,
       ...
     }:
     let
-      inherit (nixpkgs) lib;
-
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
       python = pkgs.python312;
@@ -25,25 +22,7 @@
             pkgs.uv
             pkgs.tectonic
           ];
-          env =
-            {
-              # Prevent uv from managing Python downloads
-              UV_PYTHON_DOWNLOADS = "never";
-              # Force uv to use nixpkgs Python interpreter
-              UV_PYTHON = python.interpreter;
-
-              SOURCE_DATE_EPOCH = self.sourceInfo.lastModified;
-            }
-            // lib.optionalAttrs pkgs.stdenv.isLinux {
-              # Python libraries often load native shared objects using dlopen(3).
-              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-            };
-          shellHook = ''
-            unset PYTHONPATH
-          '';
         };
       };
     };
 }
-
